9bfd779afbe980fef2a08b92588f6c3183742eb7
Mouli55555 <22a91a1280@aec.edu.in>
2025-12-22 23:04:11 +0530

    Initial commit project structure

---
+++ tenantFlow/src/main/java/com/mouli/tenantFlow/TenantFlowApplication.java
@@ -0,0 +1,13 @@
+package com.mouli.tenantFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantFlowApplication.class, args);
+	}
+
+}
---
+++ tenantFlow/src/test/java/com/mouli/tenantFlow/TenantFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouli.tenantFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
